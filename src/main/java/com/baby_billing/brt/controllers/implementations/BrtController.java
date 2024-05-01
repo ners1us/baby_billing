a3fe660cca540da5ea6eb9d446be8727d47d30e6
ners1us <66124861+ners1us@users.noreply.github.com>
2024-05-01 20:46:23 +0300

    feat: add brt controller

---
+++ src/main/java/com/baby_billing/brt/controllers/implementations/BrtController.java
@@ -0,0 +1,32 @@
+package com.baby_billing.brt.controllers.implementations;
+
+import com.baby_billing.brt.controllers.IBrtController;
+import com.baby_billing.brt.entities.BrtHistory;
+import com.baby_billing.brt.publishers.BrtToHrsRabbitMQPublisher;
+import com.baby_billing.brt.services.IBrtDatabaseService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@AllArgsConstructor
+public class BrtController implements IBrtController {
+
+    private final BrtToHrsRabbitMQPublisher brtToHrsRabbitMQPublisher;
+
+    private final IBrtDatabaseService brtDatabaseService;
+
+    @PostMapping("/sendHistoryToHrs")
+    public ResponseEntity<String> sendHistoryToHrs(@RequestParam Long historyId) {
+
+        BrtHistory brtHistory = brtDatabaseService.findBrtHistoryById(historyId);
+
+        brtToHrsRabbitMQPublisher.sendCallToHrs(brtHistory);
+
+        return ResponseEntity.ok("History sent to Hrs successfully.");
+    }
+}
