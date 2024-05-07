ee27ff259c7617fc284266d0be7742fca26960d5
ners1us <66124861+ners1us@users.noreply.github.com>
2024-05-07 23:00:36 +0300

    feat: add no args constructor

--- src/main/java/com/baby_billing/hrs/models/MonthCost.java
+++ src/main/java/com/baby_billing/hrs/models/MonthCost.java
@@ -2,11 +2,13 @@ package com.baby_billing.hrs.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class MonthCost {
 
