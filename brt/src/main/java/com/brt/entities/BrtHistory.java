804786d05e05110a05f6347a03ca3aabe803478d
ners1us <66124861+ners1us@users.noreply.github.com>
2024-06-21 15:53:36 +0300

    refactor: rename table to brt_history

--- brt/src/main/java/com/brt/entities/BrtHistory.java
+++ brt/src/main/java/com/brt/entities/BrtHistory.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
  */
 @Entity
 @Data
-@Table(name = "history")
+@Table(name = "brt_history")
 public class BrtHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
