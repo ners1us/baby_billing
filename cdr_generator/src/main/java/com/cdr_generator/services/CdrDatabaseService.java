7a6b4abd434f268c6fcd1b2b25a327726207c45a
ners1us <66124861+ners1us@users.noreply.github.com>
2024-07-15 14:15:36 +0300

    chore: remove redundant methods

--- cdr_generator/src/main/java/com/cdr_generator/services/CdrDatabaseService.java
+++ cdr_generator/src/main/java/com/cdr_generator/services/CdrDatabaseService.java
@@ -1,6 +1,5 @@
 package com.cdr_generator.services;
 
-import com.cdr_generator.entities.Client;
 import com.cdr_generator.entities.CdrHistory;
 
 import java.util.List;
@@ -8,8 +7,4 @@ import java.util.List;
 public interface CdrDatabaseService {
 
     void saveCdrToDatabase(List<CdrHistory> cdrHistoryList);
-
-    void saveClientsToDatabase(List<Client> clients);
-
-    long countClients();
 }
