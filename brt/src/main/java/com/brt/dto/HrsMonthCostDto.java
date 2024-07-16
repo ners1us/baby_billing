4294cdb5b05a331c4491e86be442aeec4fc320f6
ners1us <66124861+ners1us@users.noreply.github.com>
2024-07-16 21:15:48 +0300

    chore: remove redundant javadoc

--- brt/src/main/java/com/brt/dto/HrsMonthCostDto.java
+++ brt/src/main/java/com/brt/dto/HrsMonthCostDto.java
@@ -7,10 +7,6 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-
-/**
- * Модель для представления стоимости за месяц.
- */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
