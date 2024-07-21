fe3d6021e066a3a34d867fd2b983d993456e2936
ners1us <66124861+ners1us@users.noreply.github.com>
2024-07-21 18:26:48 +0300

    feat: add new method

--- brt/src/main/java/com/brt/repositories/BrtClientRepository.java
+++ brt/src/main/java/com/brt/repositories/BrtClientRepository.java
@@ -4,6 +4,10 @@ import com.brt.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BrtClientRepository extends JpaRepository<Client, String> {
+
+    Optional<Client> findByClientId(String phoneNumber);
 }
